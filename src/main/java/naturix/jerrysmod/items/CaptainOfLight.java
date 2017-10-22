5153bad52b368b482b54edbbe552bfbc45a71424
NicosaurusRex99 <pusspot1999@gmail.com>
2017-10-22 18:43:16 +1030

    made progress on the captain of light

--- src/main/java/naturix/jerrysmod/items/CaptainOfLight.java
+++ src/main/java/naturix/jerrysmod/items/CaptainOfLight.java
@@ -1,23 +1,23 @@
 package naturix.jerrysmod.items;
 
 import naturix.jerrysmod.JerrysMod;
-import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CaptainOfLight extends Item {
-
+	private BlockPos blockPos;
     public CaptainOfLight() {
         setRegistryName("captainoflight");        // The unique name (within your mod) that identifies this item
         setUnlocalizedName(JerrysMod.MODID + ".captainoflight");     // Used for localization (en_US.lang)
@@ -34,13 +34,16 @@ public class CaptainOfLight extends Item {
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer entity, EnumHand hand) {
 		float var4 = 1.0F;
 		ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
-		int i = (int) (entity.posX + (entity.posX - entity.prevPosX) * (double) var4);
-		int j = (int) (entity.posY + (entity.posY - entity.prevPosY) * (double) var4 + 1.62D);
-		int k = (int) (entity.posZ + (entity.posZ - entity.prevPosZ) * (double) var4);
+		RayTraceResult result = Minecraft.getMinecraft().objectMouseOver;
+        blockPos = result.getBlockPos();
+		int i = (int) (blockPos.getX() * (double) var4);
+		int j = (int) (blockPos.getY() * (double) var4 + 1.62D);
+		int k = (int) (blockPos.getZ() * (double) var4);
 
 		if (true) {
 			world.spawnEntity(new EntityLightningBolt(world, i, j, k, false));
 		}
+
 
 		return ar;
 	}
