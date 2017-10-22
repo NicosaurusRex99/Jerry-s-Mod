0125a4d0fc01d5849ea096c429021c73ef0b1498
NicosaurusRex99 <pusspot1999@gmail.com>
2017-10-22 16:46:01 +1030

    Updated Version number

--- src/main/java/naturix/jerrysmod/JerrysMod.java
+++ src/main/java/naturix/jerrysmod/JerrysMod.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class JerrysMod
 {
     public static final String MODID = "jerrysmod";
-    public static final String VERSION = "1.12.2.0B";
+    public static final String VERSION = "1.12.2.0C";
     
 
     @SidedProxy(clientSide = "naturix.jerrysmod.proxy.ClientProxy", serverSide = "naturix.jerrysmod.proxy.ServerProxy")
