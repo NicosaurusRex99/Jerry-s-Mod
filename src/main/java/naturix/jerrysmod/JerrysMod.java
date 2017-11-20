33c662853b8d21ffe50b0d182c817ffb4d32f894
NicosaurusRex99 <pusspot1999@gmail.com>
2017-11-21 01:26:30 +1030

    Added GNU license

--- src/main/java/naturix/jerrysmod/JerrysMod.java
+++ src/main/java/naturix/jerrysmod/JerrysMod.java
@@ -1,3 +1,17 @@
+/*
+    Jerry's Mod - A Minecraft Mod
+    Copyright (C) 2016-2017 Hawaii_Beach
+    This program Is free software: you can redistribute it And/Or modify
+    it under the terms Of the GNU General Public License As published by
+    the Free Software Foundation, either version 3 Of the License, Or
+    (at your option) any later version.
+    This program Is distributed In the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty Of
+    MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License For more details.
+    You should have received a copy Of the GNU General Public License
+    along with this program.  If Not, see <http://www.gnu.org/licenses/>.
+    */
 package naturix.jerrysmod;
 
 import naturix.jerrysmod.command.DimensionTp;
@@ -25,7 +39,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class JerrysMod
 {
     public static final String MODID = "jerrysmod";
-    public static final String VERSION = "1.12.2.0E";
+    public static final String VERSION = "1.12.2.0F";
     public static final String NAME = "Jerry's Mod";
     
 
--- src/main/java/naturix/jerrysmod/items/SlimeStick.java
+++ src/main/java/naturix/jerrysmod/items/SlimeStick.java
@@ -1,3 +1,17 @@
+/*
+    Jerry's Mod - A Minecraft Mod
+    Copyright (C) 2016-2017 Hawaii_Beach
+    This program Is free software: you can redistribute it And/Or modify
+    it under the terms Of the GNU General Public License As published by
+    the Free Software Foundation, either version 3 Of the License, Or
+    (at your option) any later version.
+    This program Is distributed In the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty Of
+    MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License For more details.
+    You should have received a copy Of the GNU General Public License
+    along with this program.  If Not, see <http://www.gnu.org/licenses/>.
+    */
 package naturix.jerrysmod.items;
 
 import naturix.jerrysmod.JerrysMod;
@@ -6,6 +20,7 @@ import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class SlimeStick extends Item {
 
@@ -18,6 +33,9 @@ public class SlimeStick extends Item {
     @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+    public void initOreDict() {
+    	OreDictionary.registerOre("stickWood", this);
     }
 
 }
