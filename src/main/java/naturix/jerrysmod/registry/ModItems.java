012806258d098b4da6a9bfb373bd7a94848bcd1d
NicosaurusRex99 <pusspot1999@gmail.com>
2017-12-02 22:33:09 +1030

    Added tools

--- src/main/java/naturix/jerrysmod/registry/ModItems.java
+++ src/main/java/naturix/jerrysmod/registry/ModItems.java
@@ -18,8 +18,14 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import naturix.jerrysmod.JerrysMod;
 import naturix.jerrysmod.armour.ItemModArmour;
 import naturix.jerrysmod.items.CaptainOfLight;
+import naturix.jerrysmod.items.ItemModAxe;
+import naturix.jerrysmod.items.ItemModHoe;
+import naturix.jerrysmod.items.ItemModPickaxe;
+import naturix.jerrysmod.items.ItemModSpade;
+import naturix.jerrysmod.items.ItemModSword;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
@@ -41,10 +47,24 @@ public final class ModItems {
 	    public static Item jerryLeggings;
 	    public static Item jerryBoots;
 	    public static Item slimeHead;
+	    public static Item slimeSword;
+	    public static Item slimePickaxe;
+	    public static Item slimeAxe;
+	    public static Item slimeSpade;
+	    public static Item slimeHoe;
+	    public static Item jerrySword;
+	    public static Item jerryPickaxe;
+	    public static Item jerryAxe;
+	    public static Item jerrySpade;
+	    public static Item jerryHoe;
 	    
 	    
 	    public static ArmorMaterial SLIME_ARMOR = EnumHelper.addArmorMaterial("SLIME_ARMOR", 15, new int[]{18, 51, 21, 30}, 9);
 	    public static ArmorMaterial JERRY_ARMOR = EnumHelper.addArmorMaterial("JERRY_ARMOR", 15, new int[]{40, 1658961, 3564, 230}, 59);
+	    public static ToolMaterial SLIME = EnumHelper.addToolMaterial("SLIME", 3, 1000, 15.0F, 4.0F, 30);
+	    public static ToolMaterial JERRY = EnumHelper.addToolMaterial("JERRY", 5, 10000, 150.0F, 40.0F, 300);
+	    
+	    
 	    public static final void init() {
 	        slimeShard = new Item().setUnlocalizedName("slimeShard").setCreativeTab(ModItems.JerrysModTab).setTextureName(JerrysMod.MODID + ":slimeshard");;
 	        slimeGem = new Item().setUnlocalizedName("slimeGem").setCreativeTab(ModItems.JerrysModTab).setTextureName(JerrysMod.MODID + ":slimegem");;
@@ -52,8 +72,20 @@ public final class ModItems {
 	        slimeStick = new Item().setUnlocalizedName("slimeStick").setCreativeTab(ModItems.JerrysModTab).setTextureName(JerrysMod.MODID + ":slimestick");;
 	        slimeHead = new Item().setUnlocalizedName("slimeHead").setCreativeTab(ModItems.JerrysModTab).setTextureName(JerrysMod.MODID + ":slimehead");;
 	        captainOfLight = new CaptainOfLight(null, JERRY_ARMOR, null, 0).setUnlocalizedName("captainOfLight").setCreativeTab(ModItems.JerrysModTab).setTextureName(JerrysMod.MODID + ":captainOfLight");;
-	               
-	        	        
+	        
+	        
+	        GameRegistry.registerItem(slimeSword = new ItemModSword("slime_sword", SLIME), "slime_sword");       
+	        GameRegistry.registerItem(slimePickaxe = new ItemModPickaxe("slime_pickaxe", SLIME), "slime_pickaxe");       
+	        GameRegistry.registerItem(slimeAxe = new ItemModAxe("slime_axe", SLIME), "slime_axe");       
+	        GameRegistry.registerItem(slimeSpade = new ItemModSpade("slime_spade", SLIME), "slime_spade");       
+	        GameRegistry.registerItem(slimeHoe = new ItemModHoe("slime_hoe", SLIME), "slime_hoe");       
+	        GameRegistry.registerItem(jerrySword = new ItemModSword("jerry_sword", JERRY), "jerry_sword");       
+	        GameRegistry.registerItem(jerryPickaxe = new ItemModPickaxe("jerry_pickaxe", JERRY), "jerry_pickaxe");       
+	        GameRegistry.registerItem(jerryAxe = new ItemModAxe("jerry_axe", JERRY), "jerry_axe");       
+	        GameRegistry.registerItem(jerrySpade = new ItemModSpade("jerry_spade", JERRY), "jerry_spade");       
+	        GameRegistry.registerItem(jerryHoe = new ItemModHoe("jerry_hoe", JERRY), "jerry_hoe");       
+	        
+	        
 	        GameRegistry.registerItem(slimeShard, "slimeShard");
 	        GameRegistry.registerItem(slimeGem, "slimeGem");
 	        GameRegistry.registerItem(jerryGem, "jerryGem");
