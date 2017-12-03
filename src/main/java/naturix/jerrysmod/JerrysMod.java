abb7b6fb4f68614a490d9b7ee46f4e5fb4861c61
NicosaurusRex99 <pusspot1999@gmail.com>
2017-12-03 18:44:58 +1030

    version

--- src/main/java/naturix/jerrysmod/JerrysMod.java
+++ src/main/java/naturix/jerrysmod/JerrysMod.java
@@ -39,7 +39,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class JerrysMod
 {
     public static final String MODID = "jerrysmod";
-    public static final String VERSION = "1.12.2.2";
+    public static final String VERSION = "1.12.2.3";
     public static final String NAME = "Jerry's Mod";
     
 
