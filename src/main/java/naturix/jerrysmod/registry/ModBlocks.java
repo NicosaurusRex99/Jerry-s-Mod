e9a8d8bbbd351f37e7da2c0ab294aebbd049e97b
NicosaurusRex99 <pusspot1999@gmail.com>
2017-12-04 14:20:24 +1030

    Fixed textures and llocals

--- src/main/java/naturix/jerrysmod/registry/ModBlocks.java
+++ src/main/java/naturix/jerrysmod/registry/ModBlocks.java
@@ -15,6 +15,7 @@
 package naturix.jerrysmod.registry;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import naturix.jerrysmod.JerrysMod;
 import naturix.jerrysmod.blocks.SlimeOre;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -24,6 +25,6 @@ public final class ModBlocks {
     public static Block SlimeOre;
 
     public static final void init() {
-        GameRegistry.registerBlock(SlimeOre = new SlimeOre("SlimeOre", Material.rock), "SlimeOre");
+        GameRegistry.registerBlock(SlimeOre = new SlimeOre("SlimeOre", Material.rock), "SlimeOre").setBlockTextureName(JerrysMod.MODID + ":slimeore");;
     }
 }
