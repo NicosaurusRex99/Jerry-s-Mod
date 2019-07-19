d38ada49c06183382c3d870f601f467b22929cc1
NicosaurusRex99 <pusspot1999@gmail.com>
2019-07-19 12:15:39 +0930

    fixed armor

--- src/main/java/naturix/jerrysmod/registry/ModItems.java
+++ src/main/java/naturix/jerrysmod/registry/ModItems.java
@@ -22,15 +22,15 @@ public class ModItems {
     public static Item slimeHead = new ItemBase("slime_head");
     public static Item slimeShard = new ItemBase("slime_shard");
 
-    public static ModMaterials slimeHelmetMat = new ModMaterials("slime_helmet", 5500001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
-    public static ModMaterials slimeChestplateMat = new ModMaterials("slime_chestplate", 8000001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
-    public static ModMaterials slimeLeggingsMat = new ModMaterials("slime_leggings", 7500001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
-    public static ModMaterials slimeBootsMat = new ModMaterials("slime_boots", 6500001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
+    public static ModMaterials slimeHelmetMat = new ModMaterials("slime", 5500001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
+    public static ModMaterials slimeChestplateMat = new ModMaterials("slime", 8000001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
+    public static ModMaterials slimeLeggingsMat = new ModMaterials("slime", 7500001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
+    public static ModMaterials slimeBootsMat = new ModMaterials("slime", 6500001, new int[]{18, 51, 21, 30}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(slimeGem)));
 
-    public static ModMaterials jerryHelmetMat = new ModMaterials("jerry_helmet", 11000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
-    public static ModMaterials jerryChestplateMat = new ModMaterials("jerry_chestplate", 160000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
-    public static ModMaterials jerryLeggingsMat = new ModMaterials("jerry_leggings", 15000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
-    public static ModMaterials jerryBootsMat = new ModMaterials("jerry_boots", 13000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
+    public static ModMaterials jerryHelmetMat = new ModMaterials("jerry", 11000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
+    public static ModMaterials jerryChestplateMat = new ModMaterials("jerry", 160000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
+    public static ModMaterials jerryLeggingsMat = new ModMaterials("jerry", 15000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
+    public static ModMaterials jerryBootsMat = new ModMaterials("jerry", 13000001, new int[]{506013, 1658961, 341564, 454230}, 59, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, Ingredient.fromStacks(new ItemStack(jerryGem)));
 
     public static Item slimeHelmet = new ArmorBase("slime_helmet", slimeHelmetMat, EquipmentSlotType.HEAD);
     public static Item slimeChestplate = new ArmorBase("slime_chestplate", slimeChestplateMat, EquipmentSlotType.CHEST);
@@ -41,7 +41,6 @@ public class ModItems {
     public static Item jerryChestplate = new ArmorBase("jerry_chestplate", jerryChestplateMat, EquipmentSlotType.CHEST);
     public static Item jerryLeggings = new ArmorBase("jerry_leggings", jerryLeggingsMat, EquipmentSlotType.LEGS);
     public static Item jerryBoots = new ArmorBase("jerry_boots", jerryBootsMat, EquipmentSlotType.FEET);
-
 
     public static Item slimeSword = new SwordBase("slime_sword", ModTier.SLIMESWORD, 128, ModTier.SLIMESWORD.getEfficiency());
     public static Item jerrySword = new SwordBase("jerry_sword", ModTier.JERRYSWORD, 848, ModTier.JERRYSWORD.getEfficiency());
