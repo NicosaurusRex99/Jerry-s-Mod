0927613db5b70adba3d76852321fa17122e0e07b
NicosaurusRex99 <pusspot1999@gmail.com>
2019-07-20 16:37:11 +0930

    Captain of light updates

--- src/main/java/naturix/jerrysmod/objects/items/CaptainOfLight.java
+++ src/main/java/naturix/jerrysmod/objects/items/CaptainOfLight.java
@@ -1,39 +1,41 @@
 package naturix.jerrysmod.objects.items;
 
-import net.minecraft.client.util.ITooltipFlag;
+import javafx.scene.effect.Light;
+import naturix.jerrysmod.JerrysMod;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
-import javax.annotation.Nullable;
-import java.util.List;
 
 public class CaptainOfLight extends ItemBase {
 
     public CaptainOfLight(String name) {
-        super(name);
-    }
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
-    if(world.isRemote) {
-        LightningBoltEntity bolt = new LightningBoltEntity(world, player.getLookVec().getX(), player.getLookVec().getY(), player.getLookVec().getZ(), false);
-        bolt.setLocationAndAngles(player.getLookVec().getX(), player.getLookVec().getY(), player.getLookVec().getZ(), player.rotationYaw, player.rotationPitch);
-        world.addEntity(bolt);
-    }
-    return new ActionResult<>(ActionResultType.PASS, player.getHeldItem(hand));
-    }
-    @OnlyIn(Dist.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-    tooltip.add(new StringTextComponent("This item is not ready as of yet."));
+        super(name, new Item.Properties().maxStackSize(1).group(JerrysMod.setup.itemGroup));
     }
 
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand handIn) {
+        ItemStack itemstack = player.getHeldItem(handIn);
+        Vec3d start  = player.getEyePosition(1);
+        Vec3d end = start.add(player.getLookVec().x * 100, player.getLookVec().y * 100, player.getLookVec().z * 100);
+        BlockPos pos = new BlockPos(end);
+            if (!world.isRemote) {
+                //FIXME - Currently does not spawn a lightning bolt
+                LightningBoltEntity boltEntity = new LightningBoltEntity(world, pos.getX(), pos.getY(), pos.getZ(), true);
+                boltEntity.setLocationAndAngles(pos.getX(), pos.getY(), pos.getZ(), player.rotationYaw, player.rotationPitch);
+                world.addEntity(boltEntity);
+            }
+            player.swingArm(handIn);
+            player.addStat(Stats.ITEM_USED.get(this));
+
+        return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
+    }
 }
