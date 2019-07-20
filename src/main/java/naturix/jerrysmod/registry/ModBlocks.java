521d62954f2830508f105cefc101a969f7d3a9b4
NicosaurusRex99 <pusspot1999@gmail.com>
2019-07-20 14:31:19 +0930

    added assets/data

--- src/main/java/naturix/jerrysmod/registry/ModBlocks.java
+++ src/main/java/naturix/jerrysmod/registry/ModBlocks.java
@@ -17,7 +17,7 @@ public class ModBlocks {
     public static BlockBase slime_ore = new BlockBase("slime_ore", 2, ToolType.PICKAXE);
     public static BlockBase olive_planks = new BlockBase("olive_planks", 1, ToolType.AXE);
     public static LogBase slime_log = new LogBase("slime_log");
-    public static BlockBase slime_leaves = new BlockBase("slime_leaves");
+    public static LeavesBase slime_leaves = new LeavesBase("slime_leaves");
     public static SaplingBase slime_sapling = new SaplingBase("slime_sapling", new OliveTree());
 
 }
