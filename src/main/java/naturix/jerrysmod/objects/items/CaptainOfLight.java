575a555855ae9b504abcc230054c13b014aeed07
NicosaurusRex99 <pusspot1999@gmail.com>
2020-07-16 01:15:36 +0930

    Update CaptainOfLight.java

--- src/main/java/naturix/jerrysmod/objects/items/CaptainOfLight.java
+++ src/main/java/naturix/jerrysmod/objects/items/CaptainOfLight.java
@@ -1,9 +1,22 @@
 package naturix.jerrysmod.objects.items;
 
+import com.sun.javafx.geom.Vec3d;
 import naturix.jerrysmod.JerrysMod;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
@@ -11,6 +24,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 
@@ -22,26 +36,27 @@ public class CaptainOfLight extends ItemBase {
 
 
     //TODO - Fix captain of light
-//    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand handIn) {
-//        ItemStack itemstack = player.getHeldItem(handIn);
-//        double blockReachDistance = 128;
-//        BlockRayTraceResult rayTrace = rayTraceEyes(player, blockReachDistance + 1);
-//        if (rayTrace.getType() == RayTraceResult.Type.BLOCK && !world.isRemote) {
-//            world.addEntity(new LightningBoltEntity(world, rayTrace.getPos().getX(), rayTrace.getPos().getY(), rayTrace.getPos().getZ(), false));
-//        }
-//            player.addStat(Stats.ITEM_USED.get(this));
-//
-//            return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
-//        }
-//
-//        @Nonnull
-//        public static BlockRayTraceResult rayTraceEyes(LivingEntity entity,double length){
-//            Vec3d startPos = new Vec3d(entity.getPosition().getX(), entity.getPosition().getY() + entity.getEyeHeight(), entity.getPosition().getZ());
-//            Vec3d endPos = startPos.add(new Vec3d(entity.getLookVec().x * length, entity.getLookVec().y * length, entity.getLookVec().z * length));
-//            RayTraceContext context = new RayTraceContext(startPos, endPos, RayTraceContext.BlockMode.COLLIDER,
-//                    RayTraceContext.FluidMode.NONE, entity);
-//            return entity.world.rayTraceBlocks(context);
-//        }
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand handIn) {
+        ItemStack itemstack = player.getHeldItem(handIn);
+        float blockReachDistance = 128;
+        BlockRayTraceResult rayTrace = rayTraceEyes(player, blockReachDistance + 1);
+        if (rayTrace.getType() == RayTraceResult.Type.BLOCK && !world.isRemote) {
+            LightningBoltEntity ent = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
+            BlockPos blotPos = rayTraceEyes(player, 128).getPos();
+            ent.setPosition(blotPos.getX(), blotPos.getY(), blotPos.getZ());
+            world.addEntity(ent);
+        }
+            player.addStat(Stats.ITEM_USED.get(this));
+
+            return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
+        }
+
+        @Nonnull
+        public static BlockRayTraceResult rayTraceEyes(LivingEntity entity, float length){
+            RayTraceContext context = new RayTraceContext(entity.getLookVec(), entity.getLook(length), RayTraceContext.BlockMode.COLLIDER,
+                    RayTraceContext.FluidMode.NONE, entity);
+            return entity.world.rayTraceBlocks(context);
+        }
 
 
     @OnlyIn(Dist.CLIENT)
